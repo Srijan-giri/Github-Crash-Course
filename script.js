8f61faccdb4abdf46a3ca7a103e23ffa547e1bc5
Srijan Giri <srijangiri2003@gmail.com>
2024-12-28 20:13:40 +0530

    feature b added

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ function newFunctionAdd(){
     console.log("New function added");
     console.log("New function added A");
 }
+
+function newFunctionAdd2(){
+    console.log("New function added");
+    console.log("New function added B");
+}
