59cdf71db6ddf88b23703eb7a7d87040600c3a42
Srijan Giri <srijangiri2003@gmail.com>
2024-12-28 13:17:28 +0530

    Add isAbove18 fn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const age = 24;
-const name = 'Piyush Garg';
+const name1 = 'Piyush Garg';
 const dob = '01/01/2000';
 
 function sayName() {
-    console.log(`My name is ${name}`);
+    console.log(`My name is ${name1}`);
+}
+
+
+function isAbove18(){
+    return age>=18;
 }
